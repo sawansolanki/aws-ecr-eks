689362a6d1af86d4542dfb251423e991374f9f67
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2022-12-15 16:49:19 +0530

    Update module-dependency.sh

--- module-dependency.sh
+++ module-dependency.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo apt-get install zip unzip
-unzip awscliv2.zip
 sudo apt-get update
+sudo apt-get install zip unzip
 unzip awscliv2.zip
 sudo ./aws/install
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
