7ca77c9597c6c76d4e18de97a8dbe6b03888a793
sawan solanki <64569965+sawansolanki@users.noreply.github.com>
2022-12-16 10:49:16 +0530

    Update eks-cluster-script.sh

--- eks-cluster-script.sh
+++ eks-cluster-script.sh
@@ -5,7 +5,7 @@ output=$(aws eks describe-cluster --name ${EKS_CLUSTER_NAME} 2>&1)
 
 if [ $? -ne 0 ]; then
   if echo ${output} | grep -q ResourceNotFoundException; then
-        eksctl create cluster -f ${{env.ROOT_PATH}}/cluster.yaml
+        eksctl create cluster -f cluster.yaml
   else
     >&2 echo "Error is -->" ${output}
   fi
